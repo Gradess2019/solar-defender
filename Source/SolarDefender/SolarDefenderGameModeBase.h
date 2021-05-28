cdb2061aa964516f5f49d4f766539a2984bff757
Gradess <trofimovjava@gmail.com>
2021-05-28 23:26:12 +0300

    Init

---
+++ Source/SolarDefender/SolarDefenderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SolarDefenderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOLARDEFENDER_API ASolarDefenderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
